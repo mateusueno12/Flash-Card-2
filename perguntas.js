50298a93c78a08f5dcd8478d6742aba5d3cc933b
mateusueno12 <ueno.fernandes@escola.pr.gov.br>
2025-07-29 09:17:05 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Paraná?',
+    'A capital do Paraná é Curitiba '
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa Welcome?',
+    'Welcome significa bem-vindo(a)'
+)
